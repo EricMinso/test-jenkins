bf627f2a514a7044bff09568626f6b4d2529c5b3
unknown <eminso@Shannon-7.softeam.com>
2015-04-27 14:39:36 +0200

    test

--- src/test/java/org/deverson/MainTest.java
+++ src/test/java/org/deverson/MainTest.java
@@ -2,13 +2,15 @@ package org.deverson;
 
 import static org.junit.Assert.*;
 
+import java.util.logging.Logger;
 import org.junit.Test;
 
 public class MainTest {
 
     @Test
     public void test() {
-        fail("Not yet implemented");
+        // fail("Not yet implemented");
+		log.info("Ce test fonctionne parfaitement !");
     }
 
 }
