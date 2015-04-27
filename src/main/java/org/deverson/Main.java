b101c0d2e3b7336fe865d6dcd0f898ccc1d5d91e
unknown <eminso@Shannon-7.softeam.com>
2015-04-27 15:23:07 +0200

    encore un commit

--- src/main/java/org/deverson/Main.java
+++ src/main/java/org/deverson/Main.java
@@ -1,13 +1,12 @@
 package org.deverson;
 
-//import java.util.logging.Logger;
+import java.util.logging.Logger;
 
 public class Main {
-    //private static final Logger log = Logger.getLogger(Main.class.getName());
+    private static final Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        //log.info("Hello world!");
-        //log.info("Modification...");
-		System.out.println( "Hello Main World !" );
+        log.info("log.info = Hello Main World!");
+		System.out.println( "System.out.println = Hello Main World !" );
     }
 }
--- src/test/java/org/deverson/MainTest.java
+++ src/test/java/org/deverson/MainTest.java
@@ -2,17 +2,17 @@ package org.deverson;
 
 import static org.junit.Assert.*;
 
-//import java.util.logging.Logger;
+import java.util.logging.Logger;
 import org.junit.Test;
 
 public class MainTest {
+    private static final Logger log = Logger.getLogger(Main.class.getName());
 
     @Test
     public void test() {
         // fail("Not yet implemented");
-		//log.info("Ce test fonctionne parfaitement !");
-        //log.info("Modification...");
-		System.out.println( "Hello Test World ! " );
+		log.info("log.info = Hello Test World ! ");
+		System.out.println( "System.out.println = Hello Test World ! " );
     }
 
 }
