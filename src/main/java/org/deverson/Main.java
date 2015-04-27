c04fb9f2aae20faa50918ff2a1e9001bd8925770
Cyril Deverson <cdeverson@genigraph.fr>
2015-04-27 09:15:02 +0200

    First commit: project initialization

---
+++ src/main/java/org/deverson/Main.java
@@ -0,0 +1,11 @@
+package org.deverson;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
